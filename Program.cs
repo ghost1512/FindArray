61bdf0316e17e88262173ea7d2741eda0c735829
BaoHG <baohuynh291@gmail.com>
2024-04-09 19:50:24 +0700

    add array
    array

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] students = { "A", "B", "C", "D", "E", "F", "G", "H", };
+        Console.Write("Enter a student's name : ");
+        string input_name = Console.ReadLine();
+        bool isExist = false;
+        for (int i = 0; i < students.Length; i++)
+        {
+            if (students[i].Equals(input_name))
+            {
+                Console.WriteLine($"Student {students[i]} in class!!");
+                isExist = true;
+                break;
+            }
+        }
+        if (!isExist)
+        {
+            Console.WriteLine($"Student not in class!!");
+        }
+
+    }
+}
